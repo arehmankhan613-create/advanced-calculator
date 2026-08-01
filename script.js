bfb318789bc4334886c99ce7e0afd09b33d665cf
arehmankhan613-create <arehmankhan613@gmail.com>
2026-08-01 17:36:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,132 @@ themeBtn.onclick = () => {
     }
 
 };
+/* ===========================
+   Keyboard Support
+=========================== */
+
+document.addEventListener("keydown", (e) => {
+
+    if (!isNaN(e.key) || "+-*/.%".includes(e.key)) {
+        expression += e.key;
+        display.value = expression;
+    }
+
+    if (e.key === "Enter") {
+        e.preventDefault();
+
+        try {
+
+            let exp = expression;
+
+            let answer = eval(exp);
+
+            historyList.innerHTML +=
+            `<li>${expression} = ${answer}</li>`;
+
+            display.value = answer;
+
+            expression = answer.toString();
+
+        } catch {
+
+            display.value = "Error";
+            expression = "";
+
+        }
+
+    }
+
+    if (e.key === "Backspace") {
+
+        expression = expression.slice(0,-1);
+
+        display.value = expression;
+
+    }
+
+});
+
+
+/* ===========================
+Scientific Calculator
+=========================== */
+
+const scienceButtons =
+document.querySelectorAll(".science-grid button");
+
+scienceButtons.forEach(btn=>{
+
+btn.onclick=()=>{
+
+let value=btn.innerText;
+
+let num=parseFloat(display.value);
+
+switch(value){
+
+case "√":
+display.value=Math.sqrt(num);
+break;
+
+case "x²":
+display.value=Math.pow(num,2);
+break;
+
+case "x³":
+display.value=Math.pow(num,3);
+break;
+
+case "1/x":
+display.value=1/num;
+break;
+
+case "sin":
+display.value=Math.sin(num);
+break;
+
+case "cos":
+display.value=Math.cos(num);
+break;
+
+case "tan":
+display.value=Math.tan(num);
+break;
+
+case "log":
+display.value=Math.log10(num);
+break;
+
+case "ln":
+display.value=Math.log(num);
+break;
+
+case "π":
+display.value=Math.PI.toFixed(8);
+break;
+
+case "e":
+display.value=Math.E.toFixed(8);
+break;
+
+case "!":
+
+let fact=1;
+
+for(let i=1;i<=num;i++){
+
+fact*=i;
+
+}
+
+display.value=fact;
+
+break;
+
+}
+
+expression=display.value;
+
+}
+
+});
